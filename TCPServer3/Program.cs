12e64fce57aa86b626bd447577205e05163bbdf6
Diiiya <33103806+Diiiya@users.noreply.github.com>
2018-10-05 12:19:01 +0300

    WeightConvertor TCP Server

--- TCPServer3/Program.cs
+++ TCPServer3/Program.cs
@@ -1,12 +1,67 @@
-﻿using System;
+﻿using Convertor3;
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
 
 namespace TCPServer3
 {
     class Program
     {
+        private static readonly int PORT = 10;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            IPAddress localAddress = IPAddress.Loopback;
+            TcpListener serverSocket = new TcpListener(localAddress, PORT);
+            serverSocket.Start();
+            Console.WriteLine("The TCP server is ready!");
+            Console.WriteLine("Running on port " + PORT);
+            while (true)
+            {
+                TcpClient client = serverSocket.AcceptTcpClient();
+                Console.WriteLine("Incoming client ... ");
+                Task.Run(() => DoIt(client));
+            }
+        }
+
+        private static void DoIt(TcpClient client)
+        {
+            NetworkStream stream = client.GetStream();
+            StreamReader reader = new StreamReader(stream);
+            StreamWriter writer = new StreamWriter(stream);
+            while (true)
+            {
+                string request = reader.ReadLine();
+                if (request == "Stop") { break; }
+                else
+                {
+                    Console.WriteLine("Request: " + request);
+                    string[] values = request.Split(' ');
+                    string funcion = (string)values[0];
+
+                    double _response = 0;
+
+                    double a = Double.Parse(values[1]);
+
+                    ConvertorClass c = new ConvertorClass();
+
+                    if (funcion == "ToOunces")
+                    {
+                        _response = c.ConvertToOunces(a);
+                    }
+                    else if (funcion == "ToGrams")
+                    {
+                        _response = c.ConvertToGrams(a);
+                    }
+
+                    Console.WriteLine("Response: " + _response);
+                    writer.WriteLine(_response);
+                    writer.Flush();
+                }
+            }
+            client.Close();
         }
     }
 }
